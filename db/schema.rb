ec9056246b3fe7531d1bf32aef9df965c87ed50c
Sean Ho <seanho96@yahoo.com>
2022-01-03 21:05:03 +0800

    feat: add one-to-many association between users and articles from BE

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_227_132_547) do
+ActiveRecord::Schema.define(version: 20_220_103_124_727) do
   create_table 'articles', force: :cascade do |t|
     t.string 'title'
     t.text 'description'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'user_id'
   end
 
   create_table 'users', force: :cascade do |t|
