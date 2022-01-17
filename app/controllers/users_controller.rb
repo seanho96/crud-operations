0f6fc5229399b103a20699c4987dfc335eaf0ec6
Sean Ho <seanho96@yahoo.com>
2022-01-17 21:26:29 +0800

    feat: add authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -13,11 +14,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your profile has been updated"
       redirect_to @user
@@ -29,6 +28,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Meta Blog #{@user.username}"
       redirect_to articles_path
     else
@@ -39,5 +39,9 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
-    end 
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
