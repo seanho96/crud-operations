1be0b5b340ea3b439110b4fe66646c2c66a621b9
Sean Ho <seanho96@yahoo.com>
2022-01-17 21:33:23 +0800

    feat: declare controller methods as helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,13 +22,4 @@
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user
-  end
-
 end
