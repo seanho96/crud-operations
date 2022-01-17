f528ccb919b1b02240a55c8cadbe9ab8bc3c0c3a
Sean Ho <seanho96@yahoo.com>
2022-01-17 20:56:05 +0800

    feat: create and destroy sessions for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You have successfully logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:alert] = "Invalid email/password combination"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You have successfully logged out"
+    redirect_to root_path
   end
 end
