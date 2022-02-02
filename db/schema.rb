6ea5dd8cbec8eb8df4c10f82d8509cba5b0bdf9a
Sean Ho <seanho96@yahoo.com>
2022-02-02 22:10:15 +0800

    feat: many-to-many association from the BE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_133330) do
+ActiveRecord::Schema.define(version: 2022_02_01_134205) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
