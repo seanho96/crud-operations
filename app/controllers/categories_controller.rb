fadd887c2d01cfdbc86749ef2e96170a83197d29
Sean Ho <seanho96@yahoo.com>
2022-02-03 23:25:43 +0800

    feat: add category index and show views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
-  
+
   def new
     @category = Category.new
   end
@@ -21,6 +21,7 @@ class CategoriesController < ApplicationController
   
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
