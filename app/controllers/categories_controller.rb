fed8869f55768c72f8ac4e869cbe9a47d7c080fd
Sean Ho <seanho96@yahoo.com>
2022-03-08 07:45:07 +0800

    refactor: if not to unless

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -31,7 +31,7 @@ class CategoriesController < ApplicationController
   end
 
   def require_admin
-    if !(logged_in? && current_user.admin?)
+    unless(logged_in? && current_user.admin?)
       flash[:alert] = "Only admins can perform that action"
       redirect_to categories_path
     end
